74630e6f6fa494bdad2c2f407802015b7f3dbb68
Tomás Spoturno <spoturno16@gmail.com>
2024-03-28 23:32:55 -0300

    Ejercicio 3 practico 2

--- practico2.pl
+++ practico2.pl
@@ -50,3 +50,53 @@ maximo_v2([X|L], M) :- maximo_acc(L, X, M).
 % false.
 
 
+
+% Ejercicio 3
+% Defina los siguientes predicados en prolog
+% suma(+L, ?S)          S es la suma de los elementos de la lista L
+% pares(+L, ?P)         P es una lista conteniendo solo los elementos pares de la lista
+% mayores(+L, +X, ?M)   M es una lista con los elementos de L que son mayores que X
+% merge(+L1, +L2, ?L3)  L3 es el resultado de combinar ordenadamente los elementos de las listas (ordenadas) L1 y L2  
+
+suma_acc([], Ac, Ac).
+suma_acc([X|L], Ac, S) :-
+    Ac1 is Ac + X, 
+    suma_acc(L, Ac1, S).
+
+suma(L, S) :- suma_acc(L, 0, S).
+
+
+suma_v2([], 0).
+suma_v2([Head|Tail], S) :-
+    suma_v2(Tail, STail),
+    S is Head + STail.
+
+
+pares([], []).
+pares([X|L1], [X|L2]) :-
+    X1 is X mod 2,
+    X1 =:= 0,
+    pares(L1, L2).
+pares([X|L], P) :-
+    X1 is X mod 2,
+    X1 =\= 0,
+    pares(L, P).
+
+
+mayores([], _, []).
+mayores([T|H1], X, [T|H2]) :- 
+    T > X,
+    mayores(H1, X, H2).
+mayores([T|H], X, M) :-
+    T =< X,
+    mayores(H, X, M).
+
+
+merge([], L, L).
+merge(L, [], L).
+merge([T1|L1], [T2|L2], [T1|L3]) :-
+    T1 =< T2,
+    merge(L1, [T2|L2], L3).
+merge([T1|L1], [T2|L2], [T2|L3]) :-
+    T1 > T2,
+    merge([T1|L1], L2, L3).
